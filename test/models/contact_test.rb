63bba786679aade2dc80c88d2b71134d28373ea2
Luan Carlos Araldi <luan.araldi@gmail.com>
2017-05-26 09:08:32 -0300

    Configured database
    - Added database configuration (for docker)
    - Added test to test incorrect email format
    - Added test to test correct email format

--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -5,6 +5,15 @@ class ContactTest < ActiveSupport::TestCase
   test "shouldn't save contact with incorrect email format" do
     contact = Contact.new(email: "teste#teste.com")
     assert_not contact.save, "Incorrect email format given"
+
+    contact = Contact.new(email: "testeteste.com")
+    assert_not contact.save, "Incorrect email format given"
+
+    contact = Contact.new(email: "teste@teste.com.br")
+    assert contact.save, "Correct email format given"
   end
+
   
+
+
 end
